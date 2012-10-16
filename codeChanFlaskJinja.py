ca50e436e7a2b3ed6a85cae3fa9785b712f49a4b
Matthew Lewis <matt@mplewis.com>
2012-10-16 14:38:59 -0500

    first working prototype; does board retreival, parsing, and display; thread retreival and parsing

--- codeChanFlaskJinja.py
+++ codeChanFlaskJinja.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import flask
+import threadParse
+import parseForWeb
+from htmlParse import stripTags
+import urllib2
 
+app = flask.Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return flask.render_template('index.html')
 
 @app.route('/<board>/')
 def selectBoard(board):
-	
-	return render_template('board.html')
+	index = threadParse.Index(board)
+	try:
+		index.refresh()
+	except urllib2.HTTPError:
+		flask.abort(404)
+	processedIndex = parseForWeb.parseIndex(index)
+	return flask.render_template('getBoard.html', boardAbbr = board, indexData = processedIndex)
+
+@app.route('/<board>/<threadNum>/')
+def selectThread(board, threadNum):
+	thread = threadParse.Thread()
+	thread.setBoard(board)
+	thread.setNum(threadNum)
+	try:
+		thread.refresh()
+	except urllib2.HTTPError:
+		flask.abort(404)
+	processedThread = parseForWeb.parseThread(thread)
+	return flask.render_template('getThread.html', boardAbbr = board, threadData = processedThread)
 
 if __name__ == '__main__':
 	app.run()
